561b87b2e2723168c55644c82027002c8f6c4347
program-- <justin@justinsingh.me>
2022-12-19 08:32:57 -0800

    feat: canvas struct

---
+++ canvas.go
@@ -0,0 +1,26 @@
+package rapture
+
+import (
+	"image"
+	"image/color"
+)
+
+type Canvas struct {
+	grid       Grid
+	colorspace color.Model
+}
+
+func (this *Canvas) Render() image.Image {
+	img := image.NewRGBA(this.grid.Rect())
+	filled_cells := this.grid.Values()
+	for i := 0; i < len(filled_cells); i++ {
+		cell := filled_cells[i]
+		img.Set(cell.col, cell.row, this.Color(cell.val))
+	}
+
+	return img
+}
+
+func (this *Canvas) Color(val any) color.Color {
+	panic("not implemented")
+}

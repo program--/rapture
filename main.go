de686a25bf4eab7dfa18a0e474261a633e197df8
program-- <justin@justinsingh.me>
2022-12-25 01:10:00 -0800

    feat: automatic config flag parsing

--- main.go
+++ main.go
@@ -18,32 +18,8 @@ Usage: rapture -i (input) -o (output) [options]
 
 var cfg = config.RaptureConfig{}
 
-func init() {
-	flag.StringVar(&cfg.Path, "input", "", "input file")
-	flag.StringVar(&cfg.Path, "i", "", "input file")
-
-	flag.StringVar(&cfg.Output, "output", "", "output file path (.png)")
-	flag.StringVar(&cfg.Output, "o", "", "output file path (.png)")
-
-	flag.IntVar(&cfg.Width, "width", 800, "width of output")
-	flag.IntVar(&cfg.Width, "w", 800, "width of output")
-
-	flag.IntVar(&cfg.Height, "height", 800, "height of output")
-	flag.IntVar(&cfg.Height, "h", 800, "height of output")
-
-	flag.StringVar(&cfg.Prop, "property", "", "property to use from input")
-	flag.StringVar(&cfg.Prop, "p", "", "property to use from input")
-
-	flag.StringVar(&cfg.Stat, "stat", "density", "function for displaying values, can be one of: density, mean")
-	flag.StringVar(&cfg.Stat, "s", "density", "function for displaying values, can be one of: density, mean")
-
-	flag.StringVar(&cfg.Bbox, "bbox", "", "string delimited bounding box in order: xmax,xmin,ymax,ymin")
-	flag.StringVar(&cfg.Bbox, "b", "", "string delimited bounding box in order: xmax,xmin,ymax,ymin")
-
-	flag.StringVar(&cfg.Prof, "prof", "", "write cpu profile to file")
-}
-
 func main() {
+	cfg.Init()
 	flag.Parse()
 
 	if cfg.Prof != "" {
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,12 +1,53 @@
 package config
 
+import (
+	"flag"
+	"reflect"
+	"strconv"
+)
+
 type RaptureConfig struct {
-	Path   string
-	Output string
-	Stat   string
-	Prop   string
-	Width  int
-	Height int
-	Bbox   string
-	Prof   string
+	Path   string `flag:"input" usage:"input file" alias:"i"`
+	Output string `flag:"output" usage:"output file path (.png)" alias:"o"`
+	Stat   string `flag:"stat" usage:"function for displaying values, can be of one: density, mean" default:"density" alias:"s"`
+	Prop   string `flag:"property" usage:"property to use from input" alias:"p"`
+	Width  int    `flag:"width" usage:"width of output" default:"800" alias:"w"`
+	Height int    `flag:"height" usage:"height of output" default:"800" alias:"h"`
+	Bbox   string `flag:"bbox" usage:"string delimited bounding box in order: xmax, xmin, ymax, ymin" alias:"b"`
+	Prof   string `flag:"prof" usage:"write cpu profile to file"`
+}
+
+// Sets up flag calls to struct fields based on struct tags/type
+func (c *RaptureConfig) Init() {
+	typeref := reflect.TypeOf(c).Elem()
+	valref := reflect.ValueOf(c).Elem()
+	nfields := valref.NumField()
+
+	for i := 0; i < nfields; i++ {
+		field := valref.Field(i)
+		tags := typeref.Field(i).Tag
+		ptr := field.UnsafePointer()
+
+		flag_name := tags.Get("flag")
+		flag_alias := tags.Get("alias")
+		flag_default := tags.Get("default")
+		flag_usage := tags.Get("usage")
+
+		switch field.Kind() {
+		case reflect.String:
+			flag.StringVar((*string)(ptr), flag_name, flag_default, flag_usage)
+			if flag_alias != "" {
+				flag.StringVar((*string)(ptr), flag_alias, flag_default, flag_usage)
+			}
+			continue
+		case reflect.Int:
+			flag_default_int64, _ := strconv.ParseInt(flag_default, 10, 32)
+			flag_default_int := int(flag_default_int64)
+			flag.IntVar((*int)(ptr), flag_name, flag_default_int, flag_usage)
+			if flag_alias != "" {
+				flag.IntVar((*int)(ptr), flag_alias, flag_default_int, flag_usage)
+			}
+			continue
+		}
+	}
 }
